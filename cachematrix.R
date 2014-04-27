8e5774d2fe8ee2173389e4a79d52e7304d649670
Sameer Singhal <SAMEER.SINGHAL@YAHOO.COM>
2014-04-27 18:55:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions defined here
+## makeCacheMatrix function defines a list of 4 functions to set, get matrices and also setInverse and getInverse of the matrices
+## makeCacheMatrix function sets the matrix using a "super assignment" operator doing the assignment in the global environment
+## makeCacheMatrix defines getInverse function using solve to compute martix inverse using super assignment operator
 
-## Write a short comment describing this function
+## cacheSove function returns the inverse of the passed matrix either from cached global variable if it exists there and if not then computes the
+## inverse
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(mtx = matrix()) {
+        
+	inv <- NULL
+
+        setMatrix <- function(y) {
+               mtx <<- y						## sets value in global cache
+                inv <<- NULL						
+       	}
+
+        getMatrix <- function() mtx
+	
+	setInverse <- function(inverse) inv <<- solve(mtx)               ## Use sovle() to compute inverse
+
+        getInverse <- function() inv <<- solve(mtx)
+
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse,
+	     getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getInverse()   
+
+        if(!is.null(m)) {
+                message("getting cached data")   ## if inverse exists then retrieved from global cache saving time
+                return(m)
+        }
+
+        inverse <- x$getMatrix()
+        m <- solve(inverse, ...)                 ## if inverse does not exist then computed using the solve() function
+        x$setMatrix(m)
+        m
+
 }
